04b9e6306c5b7836bea339de6745b0c155633180
b0806 <tambrendan05@gmail.com>
2025-10-01 20:15:08 -0400

    merge

--- src/counter8.v
+++ src/counter8.v
@@ -9,12 +9,17 @@ module tt_um_counter8 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+assign uio_out[7:1] = 0;
+assign uio_oe  = 0;
+
 reg [7:0] bits = 0;
 wire [7:0] bits1 = bits;
 assign uo_out = bits;
 
 // tri state output
 assign uio_out[0] = uio_in[1];
+
+wire _unused = &{ena, clk, rst_n, 1'b0};
 
 always @(posedge clk or negedge rst_n) begin
 	// async reset
