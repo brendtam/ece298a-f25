5e2e19d8f1a53a9502933fe6967c8f17d8c18f3f
b0806 <tambrendan05@gmail.com>
2025-10-01 20:28:05 -0400

    update

--- test/test.py
+++ test/test.py
@@ -39,7 +39,7 @@ async def test_project(dut):
     dut.uio_in.value = 1
     await ClockCycles(dut.clk, 1)
     dut.uio_in.value = 0
-    await ClockCycles(dut.clk, 3)
+    await ClockCycles(dut.clk, 4)
     assert dut.uo_out.value == 13
 
     # tri state
