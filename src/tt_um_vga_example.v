8dc81e9f81d6deaaa5d752bf63b1fbfc1b1c96fb
b0806 <tambrendan05@gmail.com>
2025-11-10 17:58:08 -0500

    test

--- src/tt_um_vga_example.v
+++ src/tt_um_vga_example.v
@@ -61,53 +61,61 @@ module tt_um_vga_example(
   assign radius_sqrt2 = (radius >> 1) + (radius >> 3) + (radius >> 4) + (radius >> 6);
   
   assign bullet_pos_x[0] = H_ORIGIN + radius;           // 0°
-	assign bullet_pos_y[0] = V_ORIGIN;
+    assign bullet_pos_y[0] = V_ORIGIN;
 
-	assign bullet_pos_x[1] = H_ORIGIN + radius_sqrt2;     // 45°
-	assign bullet_pos_y[1] = V_ORIGIN - radius_sqrt2;
+    assign bullet_pos_x[1] = H_ORIGIN + radius_sqrt2;     // 45°
+    assign bullet_pos_y[1] = V_ORIGIN - radius_sqrt2;
 
-	assign bullet_pos_x[2] = H_ORIGIN;                    // 90°
-	assign bullet_pos_y[2] = V_ORIGIN - radius;
+    assign bullet_pos_x[2] = H_ORIGIN;                    // 90°
+    assign bullet_pos_y[2] = V_ORIGIN - radius;
 
-	assign bullet_pos_x[3] = H_ORIGIN - radius_sqrt2;     // 135°
-	assign bullet_pos_y[3] = V_ORIGIN - radius_sqrt2;
+    assign bullet_pos_x[3] = H_ORIGIN - radius_sqrt2;     // 135°
+    assign bullet_pos_y[3] = V_ORIGIN - radius_sqrt2;
 
-	assign bullet_pos_x[4] = H_ORIGIN - radius;           // 180° in_p
-	assign bullet_pos_y[4] = V_ORIGIN;
+    assign bullet_pos_x[4] = H_ORIGIN - radius;           // 180° in_p
+    assign bullet_pos_y[4] = V_ORIGIN;
 
-	assign bullet_pos_x[5] = H_ORIGIN - radius_sqrt2;     // 225°
-	assign bullet_pos_y[5] = V_ORIGIN + radius_sqrt2;
+    assign bullet_pos_x[5] = H_ORIGIN - radius_sqrt2;     // 225°
+    assign bullet_pos_y[5] = V_ORIGIN + radius_sqrt2;
 
-	assign bullet_pos_x[6] = H_ORIGIN;                    // 270°
-	assign bullet_pos_y[6] = V_ORIGIN + radius;
+    assign bullet_pos_x[6] = H_ORIGIN;                    // 270°
+    assign bullet_pos_y[6] = V_ORIGIN + radius;
 
-	assign bullet_pos_x[7] = H_ORIGIN + radius_sqrt2;     // 315°
-	assign bullet_pos_y[7] = V_ORIGIN + radius_sqrt2;
-  
+    assign bullet_pos_x[7] = H_ORIGIN + radius_sqrt2;     // 315°
+    assign bullet_pos_y[7] = V_ORIGIN + radius_sqrt2;
+  /*
   genvar i;
   generate
-	for (i = 0; i < 8; i = i + 1) begin
+    for (i = 0; i < 8; i = i + 1) begin
     wire [9:0] dx = (pix_x > bullet_pos_x[i]) ? (pix_x - bullet_pos_x[i]) : (bullet_pos_x[i] - pix_x);
     wire [9:0] dy = (pix_y > bullet_pos_y[i]) ? (pix_y - bullet_pos_y[i]) : (bullet_pos_y[i] - pix_y);
     assign bullets[i] = ((dx + dy) + ((dx > dy ? dy : dx) >> 1) <= BULLET_SIZE);
-	end
-  endgenerate
-	
-	assign in_pattern = ~(bullets == 0);
+    end
+  endgenerate */
+
+  reg [2:0] bullet_idx;
+  wire [9:0] bx = bullet_pos_x[bullet_idx];
+  wire [9:0] by = bullet_pos_y[bullet_idx];
+  wire [9:0] dx = (pix_x > bx) ? (pix_x - bx) : (bx - pix_x);
+  wire [9:0] dy = (pix_y > by) ? (pix_y - by) : (by - pix_y);
+  assign in_pattern = ((dx + dy - ((dx + dy) >> 2)) <= BULLET_SIZE);
+    
+    //assign in_pattern = ~(bullets == 0);
   assign border = ((pix_x <= 10) || (pix_x >= 630)) || ((pix_y <= 10) || (pix_y >= 470));
   
   assign R = (video_active && (in_pattern || border)) ? 2'b11 : 2'b00;
-	assign G = 2'b00;
-	assign B = (video_active && in_pattern) ? ((border) ? 2'b00 : 2'b11) : 2'b00;
+    assign G = 2'b00;
+    assign B = (video_active && in_pattern) ? ((border) ? 2'b00 : 2'b11) : 2'b00;
   
   always @(posedge vsync, negedge rst_n) begin
     if (~rst_n) begin
       radius <= 0;
     end else begin
-			radius <= radius + 20;
-			if (radius > 400) begin
-				radius <= 0;
-			end
+      bullet_idx <= bullet_idx + 1;
+            radius <= radius + 1;
+            if (radius > 400) begin
+                radius <= 0;
+            end
     end
   end
   
