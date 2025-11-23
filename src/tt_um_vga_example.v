5f234012e039d0620d281f91010400bca9100489
b0806 <tambrendan05@gmail.com>
2025-11-23 11:34:23 -0500

    test

--- src/tt_um_vga_example.v
+++ src/tt_um_vga_example.v
@@ -35,7 +35,7 @@ module tt_um_vga_example(
   reg signed [15:0] start_v;
 
   always @(*) begin
-    case(angle_idx[6:2])
+    case(angle_idx[5:2])
       0:  begin cos_val =  128; sin_val =    0; start_u = -40960; start_v = -30720; end
       1:  begin cos_val =  127; sin_val =   28; start_u = -33920; start_v = -39440; end
       2:  begin cos_val =  122; sin_val =   55; start_u = -25840; start_v = -46880; end
@@ -52,22 +52,6 @@ module tt_um_vga_example(
       13: begin cos_val = -113; sin_val =   79; start_u =  55120; start_v =   1840; end
       14: begin cos_val = -122; sin_val =   55; start_u =  52240; start_v =  11680; end
       15: begin cos_val = -127; sin_val =   28; start_u =  47360; start_v =  21520; end
-      16: begin cos_val = -128; sin_val =    0; start_u =  40960; start_v =  30720; end
-      17: begin cos_val = -127; sin_val =  -28; start_u =  33920; start_v =  39440; end
-      18: begin cos_val = -122; sin_val =  -55; start_u =  25840; start_v =  46880; end
-      19: begin cos_val = -113; sin_val =  -79; start_u =  17200; start_v =  52400; end
-      20: begin cos_val =  -99; sin_val =  -99; start_u =   7920; start_v =  55440; end
-      21: begin cos_val =  -79; sin_val = -113; start_u =  -1840; start_v =  55120; end
-      22: begin cos_val =  -55; sin_val = -122; start_u = -11680; start_v =  52240; end
-      23: begin cos_val =  -28; sin_val = -127; start_u = -21520; start_v =  47360; end
-      24: begin cos_val =    0; sin_val = -128; start_u = -30720; start_v =  40960; end
-      25: begin cos_val =   28; sin_val = -127; start_u = -39440; start_v =  33920; end
-      26: begin cos_val =   55; sin_val = -122; start_u = -46880; start_v =  25840; end
-      27: begin cos_val =   79; sin_val = -113; start_u = -52400; start_v =  17200; end
-      28: begin cos_val =   99; sin_val =  -99; start_u = -55440; start_v =   7920; end
-      29: begin cos_val =  113; sin_val =  -79; start_u = -55120; start_v =  -1840; end
-      30: begin cos_val =  122; sin_val =  -55; start_u = -52240; start_v = -11680; end
-      31: begin cos_val =  127; sin_val =  -28; start_u = -47360; start_v = -21520; end
       default: begin cos_val = 128; sin_val = 0; start_u = -40960; start_v = -30720; end
     endcase
   end
@@ -91,30 +75,40 @@ module tt_um_vga_example(
         angle_idx <= angle_idx + speed;
       end
 
-      // --- Coordinate Updates ---
-      // Priority 1: Start of Frame (Top-Left)
       if (pix_y == 0 && pix_x == 0) begin
+        if (angle_idx[6]) begin
+          row_u <= -start_u;
+          row_v <= -start_v;
+          curr_u <= -start_u;
+          curr_v <= -start_v;
+        end else begin
           row_u <= start_u;
           row_v <= start_v;
-          curr_u <= start_u; // Also prep current pixel
+          curr_u <= start_u;
           curr_v <= start_v;
-      end 
-      // Priority 2: Start of Line (Reset X, Step Y)
+        end
+      end
       else if (pix_x == 0) begin
-          // Calculate Next Row Start
-          // Moving down in Y implies adding (-sin, cos)
-          row_u <= row_u - sin_val; 
+        if (angle_idx[6]) begin
+          row_u <= row_u + sin_val;
+          row_v <= row_v - cos_val;
+          curr_u <= row_u + sin_val;
+          curr_v <= row_v - cos_val;
+        end else begin
+          row_u <= row_u - sin_val;
           row_v <= row_v + cos_val;
-          
-          // Reset pixel pointer to the NEW row start
           curr_u <= row_u - sin_val;
           curr_v <= row_v + cos_val;
-      end 
-      // Priority 3: Pixel Step (Step X)
+        end
+      end
       else if (video_active) begin
-          // Moving right in X implies adding (cos, sin)
+        if (angle_idx[6]) begin
+          curr_u <= curr_u - cos_val;
+          curr_v <= curr_v - sin_val;
+        end else begin
           curr_u <= curr_u + cos_val;
           curr_v <= curr_v + sin_val;
+        end
       end
     end
   end
