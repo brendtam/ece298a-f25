5653c16beb4405f915bf57f476b2e90fc28dd109
b0806 <tambrendan05@gmail.com>
2025-11-23 19:16:53 -0500

    test

--- src/tt_um_vga_example.v
+++ src/tt_um_vga_example.v
@@ -19,7 +19,7 @@ module tt_um_vga_example(
     // parameter NUM_BULLETS = 8;
 
     reg [9:0] fall_y;
-    reg [9:0] shift_side;
+    reg [8:0] shift_side;
 
     wire [9:0] bullet_pos_x [0:7];
     wire [9:0] bullet_pos_y [0:7];
@@ -45,8 +45,13 @@ module tt_um_vga_example(
   generate
   for (k = 0; k < 4; k++) begin
     if (k != 3) begin
-      assign bullet_pos_x[k] = H_ORIGIN + base_x_w[k] - shift_side;
-      assign bullet_pos_x[7-k] = H_ORIGIN - base_x_w[k] + shift_side;
+      if (k != 1) begin
+        assign bullet_pos_x[k] = H_ORIGIN + base_x_w[k] - shift_side;
+        assign bullet_pos_x[7-k] = H_ORIGIN - base_x_w[k] + shift_side;
+      end else begin
+        assign bullet_pos_x[k] = H_ORIGIN + base_x_w[k] - (shift_side<<1);
+        assign bullet_pos_x[7-k] = H_ORIGIN - base_x_w[k] + (shift_side<<1);
+      end
     end else begin
       assign bullet_pos_x[k] = H_ORIGIN + base_x_w[k];
     end
@@ -54,28 +59,10 @@ module tt_um_vga_example(
     assign bullet_pos_y[7-k] = base_y_w[k] + fall_y; 
   end
   endgenerate
-
-    // // Horizontal positions for a W
-    // assign bullet_pos_x[0] = H_ORIGIN - 100 - (shift_side>>1); //top left
-    // assign bullet_pos_x[1] = H_ORIGIN - 72 - shift_side;  //middle left
-    // assign bullet_pos_x[2] = H_ORIGIN - 50 - (shift_side>>1);  //bottom left
-    // assign bullet_pos_x[3] = H_ORIGIN;                    //center
-    // assign bullet_pos_x[4] = H_ORIGIN + 100 + (shift_side>>1); //top right
-    // assign bullet_pos_x[5] = H_ORIGIN + 50 + (shift_side>>1);  //bottom right
-    // assign bullet_pos_x[6] = H_ORIGIN + 72 + shift_side;  //middle right
-
-    // // Vertical positions: compute from single fall_y
-    // assign bullet_pos_y[0] = V_ORIGIN + fall_y;        // top left
-    // assign bullet_pos_y[1] = V_ORIGIN + 48 + fall_y;        // middle left
-    // assign bullet_pos_y[2] = V_ORIGIN + 100 + fall_y;   // bottom left
-    // assign bullet_pos_y[3] = V_ORIGIN + 50 + fall_y;        // center
-    // assign bullet_pos_y[4] = V_ORIGIN + fall_y;        // top right
-    // assign bullet_pos_y[5] = V_ORIGIN + 100 + fall_y;   // bottom right
-    // assign bullet_pos_y[6] = V_ORIGIN + 48 + fall_y;   // bottom right
  
     // Single fall counter, updated on vsync
    reg [9:0] frame_count; // counts vsync frames
-   reg [9:0] fall_speed;
+   reg [4:0] fall_speed;
 
 always @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
@@ -84,13 +71,11 @@ always @(posedge clk or negedge rst_n) begin
         fall_speed <= 2;
     end else if (vsync) begin
       
-        if (frame_count == 800) begin   // update every 255 clk cycles
+        if (frame_count == 800) begin   // update every 800 clk cycles
             shift_side <= (fall_y < 180) ? 0 : shift_side + 1;
 
             if      (fall_y < 25) fall_speed <= 10;
-            else if (fall_y < 75) fall_speed <= 4;
-            else if (fall_y < 100) fall_speed <= 3;
-            else if (fall_y < 130) fall_speed <= 2;
+            else if (fall_y < 125) fall_speed <= 4;
             else                   fall_speed <= 1;
 
             // Apply movement
@@ -112,8 +97,7 @@ end
         for (i = 0; i < 8; i=i+1) begin
             wire [9:0] dx = (pix_x > bullet_pos_x[i]) ? (pix_x - bullet_pos_x[i]) : (bullet_pos_x[i] - pix_x);
             wire [9:0] dy = (pix_y > bullet_pos_y[i]) ? (pix_y - bullet_pos_y[i]) : (bullet_pos_y[i] - pix_y);
-            wire [9:0] max_d = (dx > dy) ? dx : dy;
-            assign bullets[i] = (max_d + ((dx + dy) >> 2)) <= BULLET_SIZE;
+            assign bullets[i] = ((dx + dy) <= BULLET_SIZE);
         end
     endgenerate
 
@@ -125,4 +109,3 @@ end
     assign B = (video_active && in_pattern) ? ((border) ? 2'b00 : 2'b11) : 2'b00;
 
 endmodule
-
