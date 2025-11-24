6cfaaa1778e800f66ecfddc050e3da92d9c11de3
b0806 <tambrendan05@gmail.com>
2025-11-24 00:51:24 -0500

    test

--- src/tt_um_vga_example.v
+++ src/tt_um_vga_example.v
@@ -11,130 +11,322 @@ module tt_um_vga_example(
   input wire rst_n
 );
 
-  wire hsync;
-  wire vsync;
-  wire video_active;
-  wire [9:0] pix_x;
-  wire [9:0] pix_y;
+    // VGA signals
+    wire hsync, vsync;
+    wire [1:0] R, G, B;
+    wire video_active;
+    wire [9:0] pix_x, pix_y;
+    
 
-  hvsync_generator hvsync_gen(
-    .clk(clk),
-    .reset(~rst_n),
-    .hsync(hsync),
-    .vsync(vsync),
-    .display_on(video_active),
-    .hpos(pix_x),
-    .vpos(pix_y)
-  );
+    parameter H_ORIGIN = 320;
+    parameter V_ORIGIN = 0;
+    parameter BULLET_SIZE = 10;
+    // parameter NUM_BULLETS = 8;
 
-  reg signed [8:0] cos_val;
-  reg signed [8:0] sin_val;
-  reg [6:0] angle_idx;
+    reg [9:0] fall_y;
+    reg [9:0] shift_side;
 
-  reg signed [15:0] start_u;
-  reg signed [15:0] start_v;
+    wire [9:0] bullet_pos_x [0:7];
+    wire [9:0] bullet_pos_y [0:7];
+    wire [7:0] bullets;
+    
+    reg rise;
+    reg done;
 
-  always @(*) begin
-    case(angle_idx[5:2])
-      0:  begin cos_val =  128; sin_val =    0; start_u = -40960; start_v = -30720; end
-      1:  begin cos_val =  127; sin_val =   28; start_u = -33920; start_v = -39440; end
-      2:  begin cos_val =  122; sin_val =   55; start_u = -25840; start_v = -46880; end
-      3:  begin cos_val =  113; sin_val =   79; start_u = -17200; start_v = -52400; end
-      4:  begin cos_val =   99; sin_val =   99; start_u =  -7920; start_v = -55440; end
-      5:  begin cos_val =   79; sin_val =  113; start_u =   1840; start_v = -55120; end
-      6:  begin cos_val =   55; sin_val =  122; start_u =  11680; start_v = -52240; end
-      7:  begin cos_val =   28; sin_val =  127; start_u =  21520; start_v = -47360; end
-      8:  begin cos_val =    0; sin_val =  128; start_u =  30720; start_v = -40960; end
-      9:  begin cos_val =  -28; sin_val =  127; start_u =  39440; start_v = -33920; end
-      10: begin cos_val =  -55; sin_val =  122; start_u =  46880; start_v = -25840; end
-      11: begin cos_val =  -79; sin_val =  113; start_u =  52400; start_v = -17200; end
-      12: begin cos_val =  -99; sin_val =   99; start_u =  55440; start_v =  -7920; end
-      13: begin cos_val = -113; sin_val =   79; start_u =  55120; start_v =   1840; end
-      14: begin cos_val = -122; sin_val =   55; start_u =  52240; start_v =  11680; end
-      15: begin cos_val = -127; sin_val =   28; start_u =  47360; start_v =  21520; end
-      default: begin cos_val = 128; sin_val = 0; start_u = -40960; start_v = -30720; end
-    endcase
-  end
+    reg [8:0] eff_shift_x0 = shift_side;
+    reg [8:0] eff_shift_x1 = shift_side;
+    reg [8:0] eff_shift_x2 = shift_side;
+    reg [8:0] eff_shift_x3 = shift_side;
 
-  reg signed [15:0] row_u, row_v;
-  reg signed [15:0] curr_u, curr_v;
+    reg signed [9:0] cur_base_x0 = -62;
+    reg signed [9:0] cur_base_x1 = -62;
+    reg signed [9:0] cur_base_x2 = -50;
+    reg signed [9:0] cur_base_x3 = -18;
+    reg signed [9:0] cur_base_y0 = 0;
+    reg signed [9:0] cur_base_y1 = 48;
+    reg signed [9:0] cur_base_y2 = 100;
+    reg signed [9:0] cur_base_y3 = 120;
 
-  reg [5:0] speed;
-  reg [5:0] shift;
 
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      angle_idx <= 0;
-      row_u <= 0; row_v <= 0;
-      curr_u <= 0; curr_v <= 0;
-      speed <= 1;
-      shift <= 1;
-    end else begin
-      if (pix_y == 479 && pix_x == 639) begin
-        if (angle_idx + speed < angle_idx) begin
-          speed <= speed + 1;
-        end
-        angle_idx <= angle_idx + speed;
-        shift <= shift + 3;
-      end
+    // U pattern
+    localparam signed [9:0] u_base_x0 = -62;
+    localparam signed [9:0] u_base_x1 = -62;
+    localparam signed [9:0] u_base_x2 = -50;
+    localparam signed [9:0] u_base_x3 = -18;
 
-      if (pix_y == 0 && pix_x == 0) begin
-        if (angle_idx[6]) begin
-          row_u <= -start_u;
-          row_v <= -start_v;
-          curr_u <= -start_u;
-          curr_v <= -start_v;
-        end else begin
-          row_u <= start_u;
-          row_v <= start_v;
-          curr_u <= start_u;
-          curr_v <= start_v;
-        end
-      end
-      else if (pix_x == 0) begin
-        if (angle_idx[6]) begin
-          row_u <= row_u + sin_val;
-          row_v <= row_v - cos_val;
-          curr_u <= row_u + sin_val;
-          curr_v <= row_v - cos_val;
-        end else begin
-          row_u <= row_u - sin_val;
-          row_v <= row_v + cos_val;
-          curr_u <= row_u - sin_val;
-          curr_v <= row_v + cos_val;
-        end
-      end
-      else if (video_active) begin
-        if (angle_idx[6]) begin
-          curr_u <= curr_u - cos_val;
-          curr_v <= curr_v - sin_val;
-        end else begin
-          curr_u <= curr_u + cos_val;
-          curr_v <= curr_v + sin_val;
-        end
-      end
-    end
-  end
+    localparam signed [9:0] u_base_y0 = 0;
+    localparam signed [9:0] u_base_y1 = 48;
+    localparam signed [9:0] u_base_y2 = 100;
+    localparam signed [9:0] u_base_y3 = 120;
 
-  wire signed [8:0] int_u = curr_u[15:7];
-  wire signed [8:0] int_v = curr_v[15:7];
+    //W pattern
+    localparam signed [9:0] w_base_x0 = -100;
+    localparam signed [9:0] w_base_x1 = -72;
+    localparam signed [9:0] w_base_x2 = -50;
+    localparam signed [9:0] w_base_x3 = 0;
 
-  wire t_bar  = (int_u >= -40 && int_u <= 40) && (int_v >= -50 && int_v <= -30);
-  wire t_stem = (int_u >= -10 && int_u <= 10) && (int_v >= -30 && int_v <= 30);
-  wire in_shape = (t_bar || t_stem) && speed;
+    localparam signed [9:0] w_base_y0 = 0;
+    localparam signed [9:0] w_base_y1 = 48;
+    localparam signed [9:0] w_base_y2 = 100;
+    localparam signed [9:0] w_base_y3 = 50;
 
-  wire [9:0] shift_y = pix_y - shift;
+    // A pattern
+    localparam signed [9:0] a_base_x0 = 0;
+    localparam signed [9:0] a_base_x1 = 0;
+    localparam signed [9:0] a_base_x2 = -30;
+    localparam signed [9:0] a_base_x3 = -75;
 
-  wire [1:0] R = (in_shape) ? 2'b11 : 
-    ((pix_x[5] ^ shift_y[5]) ? 2'b00 : 2'b01);
-  wire [1:0] G = (in_shape) ? 2'b11 : 
-    ((pix_x[5] ^ shift_y[5]) ? 2'b00 : 2'b01);
-  wire [1:0] B = (in_shape) ? 2'b01 : 
-    ((pix_x[5] ^ shift_y[5]) ? 2'b00 : 2'b01);
+    localparam signed [9:0] a_base_y0 = 100;
+    localparam signed [9:0] a_base_y1 = 0;
+    localparam signed [9:0] a_base_y2 = 75;
+    localparam signed [9:0] a_base_y3 = 150;
 
-  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
-  assign uio_out = 0;
+
+    // E pattern
+    localparam signed [9:0] e_base_x0 = -31;
+    localparam signed [9:0] e_base_x1 = -31;
+    localparam signed [9:0] e_base_x2 = -31;
+    localparam signed [9:0] e_base_x3 = -31;
+
+    localparam signed [9:0] e_base_y0 = 0;
+    localparam signed [9:0] e_base_y1 = 60;
+    localparam signed [9:0] e_base_y2 = 120;
+    localparam signed [9:0] e_base_y3 = 120;
+
+    // R pattern
+    localparam signed [9:0] r_base_x0 = -31;
+    localparam signed [9:0] r_base_x1 = -31;
+    localparam signed [9:0] r_base_x2 = -31;
+    localparam signed [9:0] r_base_x3 = -31;
+
+    localparam signed [9:0] r_base_y0 = 0;
+    localparam signed [9:0] r_base_y1 = 48;
+    localparam signed [9:0] r_base_y2 = 100;
+    localparam signed [9:0] r_base_y3 = 152;
+
+    reg [2:0] pattern_state;
+
+    assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+    assign uio_out = 0;
   assign uio_oe  = 0;
   wire _unused_ok = &{ena, ui_in, uio_in};
 
+    // Simple VGA generator
+    hvsync_generator hvsync_gen (
+        .clk(clk),
+        .reset(~rst_n),
+        .hsync(hsync),
+        .vsync(vsync),
+        .display_on(video_active),
+        .hpos(pix_x),
+        .vpos(pix_y)
+    );
+
+    always @(*) begin
+    case(pattern_state)
+        0: begin  //U
+            cur_base_x0 = u_base_x0; 
+            cur_base_x1 = u_base_x1; 
+            cur_base_x2 = u_base_x2; 
+            cur_base_x3 = u_base_x3; 
+
+            cur_base_y0 = u_base_y0;
+            cur_base_y1 = u_base_y1;
+            cur_base_y2 = u_base_y2;
+            cur_base_y3 = u_base_y3;
+
+            eff_shift_x0 = shift_side;
+            eff_shift_x1 = shift_side;
+            eff_shift_x2 = shift_side;
+            eff_shift_x3 = shift_side;
+        end
+        1: begin   //W
+            cur_base_x0 = w_base_x0; 
+            cur_base_x1 = w_base_x1; 
+            cur_base_x2 = w_base_x2; 
+            cur_base_x3 = w_base_x3; 
+
+            cur_base_y0 = w_base_y0;
+            cur_base_y1 = w_base_y1;
+            cur_base_y2 = w_base_y2;
+            cur_base_y3 = w_base_y3;
+
+            eff_shift_x0 = (shift_side);
+            eff_shift_x1 = ((shift_side << 1));
+            eff_shift_x2 = (shift_side);
+            eff_shift_x3 = 0;
+        end
+        2: begin   //A
+          cur_base_x0 = a_base_x0; 
+          cur_base_x1 = a_base_x1; 
+          cur_base_x2 = a_base_x2; 
+          cur_base_x3 = a_base_x3; 
+
+          cur_base_y0 = a_base_y0;
+          cur_base_y1 = a_base_y1;
+          cur_base_y2 = a_base_y2;
+          cur_base_y3 = a_base_y3;
+
+          eff_shift_x0 = 0;
+          eff_shift_x1 = shift_side;
+          eff_shift_x2 = shift_side << 1;
+          eff_shift_x3 = shift_side << 1;
+        end
+        3: begin  //E
+          cur_base_x0 = e_base_x0; 
+          cur_base_x1 = e_base_x1; 
+          cur_base_x2 = e_base_x2; 
+          cur_base_x3 = e_base_x3; 
+
+          cur_base_y0 = e_base_y0;
+          cur_base_y1 = e_base_y1;
+          cur_base_y2 = e_base_y2;
+          cur_base_y3 = e_base_y3;
+
+          eff_shift_x0 = shift_side<<1;
+          eff_shift_x1 = shift_side<<2;
+          eff_shift_x2 = shift_side<<1;
+          eff_shift_x3 = shift_side<<2;
+        end
+        4: begin  //R
+          cur_base_x0 = r_base_x0; 
+          cur_base_x1 = r_base_x1; 
+          cur_base_x2 = r_base_x2; 
+          cur_base_x3 = r_base_x3; 
+
+          cur_base_y0 = r_base_y0;
+          cur_base_y1 = r_base_y1;
+          cur_base_y2 = r_base_y2;
+          cur_base_y3 = r_base_y3;
+
+          eff_shift_x0 = 10;
+          eff_shift_x1 = 20;
+          eff_shift_x2 = 10;
+          eff_shift_x3 = 60;
+        end
+        // Add more patterns
+        default: begin
+            cur_base_x0 = 0; 
+            cur_base_x1 = 0; 
+            cur_base_x2 = 0; 
+            cur_base_x3 = 0; 
+
+            cur_base_y0 = 0;
+            cur_base_y1 = 0;
+            cur_base_y2 = 0;
+            cur_base_y3 = 0;
+
+            eff_shift_x0 = 0;
+            eff_shift_x1 = 0;
+            eff_shift_x2 = 0;
+            eff_shift_x3 = 0;
+        end
+    endcase
+end
+
+  
+    // Single fall counter, updated on vsync
+   reg [9:0] frame_count; // counts vsync frames
+   reg [4:0] fall_speed;
+
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        frame_count <= 0;
+        fall_y <= 0;
+        fall_speed <= 2;
+        rise <= 0;
+    end else if (vsync) begin
+      
+        if (frame_count == 500) begin   // update every 800 clk cycles
+            // Step 1: Update fall_y according to rise/fall
+            if (rise == 1) begin
+                if (fall_y > 0) begin
+                    done <= 1;
+                    fall_y <= fall_y - 1;
+                end else begin
+                    rise <= 0;  // stop rising at 0
+                end
+            end else begin
+                if (fall_y >= 180 && pattern_state == 0)
+                    fall_y <= fall_y;  // freeze for U pattern
+                else
+                    fall_y <= fall_y + fall_speed;
+            end
+
+            // Step 2: Shift update
+            shift_side <= (fall_y >= 180) ? shift_side + 1 : (rise == 1) ? shift_side + 1 : 0 ;
+
+            // Step 3: Trigger rise if appropriate
+            if ((pattern_state == 4 || pattern_state == 3) && fall_y > 200 && done == 0) begin
+                rise <= 1;
+            end
+
+            // Step 4: Advance pattern only if fall_y reached bottom
+            if (shift_side >= 400 || (!rise && fall_y >= 600)) begin
+                done <= 0;
+                fall_y <= 0;
+                shift_side <= 0;
+                rise <= 0;
+                pattern_state <= (pattern_state == 4) ? 0 : pattern_state + 1;
+            end
+
+        
+
+            frame_count <= 0;
+
+
+        end else begin
+            frame_count <= frame_count + 1;
+        end
+    end
+end
+
+  genvar k;
+  generate
+  for (k = 0; k < 4; k = k + 1) begin
+    if (k == 0) begin
+      assign bullet_pos_x[k]   = H_ORIGIN + cur_base_x0 - eff_shift_x0;
+      assign bullet_pos_x[7-k] = H_ORIGIN - cur_base_x0 + eff_shift_x0;
+      assign bullet_pos_y[k]   = cur_base_y0 + fall_y;
+      assign bullet_pos_y[7-k] = cur_base_y0 + fall_y;
+    end else if (k == 1) begin
+      assign bullet_pos_x[k]   = H_ORIGIN + cur_base_x1 - eff_shift_x1;
+      assign bullet_pos_x[7-k] = H_ORIGIN - cur_base_x1 + eff_shift_x1;
+      assign bullet_pos_y[k]   = cur_base_y1 + fall_y;
+      assign bullet_pos_y[7-k] = cur_base_y1 + fall_y;
+    end else if (k == 2) begin
+      assign bullet_pos_x[k]   = H_ORIGIN + cur_base_x2 - eff_shift_x2;
+      assign bullet_pos_x[7-k] = H_ORIGIN - cur_base_x2 + eff_shift_x2;
+      assign bullet_pos_y[k]   = cur_base_y2 + fall_y;
+      assign bullet_pos_y[7-k] = cur_base_y2 + fall_y;
+    end else begin
+      assign bullet_pos_x[k]   = H_ORIGIN + cur_base_x3 - eff_shift_x3;
+      assign bullet_pos_x[7-k] = H_ORIGIN - cur_base_x3 + eff_shift_x3;
+      assign bullet_pos_y[k]   = cur_base_y3 + fall_y;
+      assign bullet_pos_y[7-k] = cur_base_y3 + fall_y;
+    end
+  end
+endgenerate
+
+
+
+    // Render bullets
+    genvar i;
+    generate
+        for (i = 0; i < 8; i=i+1) begin
+            wire [9:0] dx = (pix_x > bullet_pos_x[i]) ? (pix_x - bullet_pos_x[i]) : (bullet_pos_x[i] - pix_x);
+            wire [9:0] dy = (pix_y > bullet_pos_y[i]) ? (pix_y - bullet_pos_y[i]) : (bullet_pos_y[i] - pix_y);
+            assign bullets[i] = ((dx + dy) <= BULLET_SIZE);
+        end
+    endgenerate
+
+    wire in_pattern = |bullets;
+    wire border = ((pix_x <= 10) || (pix_x >= 630)) || ((pix_y <= 10) || (pix_y >= 470));
+
+    assign R = (video_active && (in_pattern || border)) ? 2'b11 : 2'b00;  // max red
+    assign G = (video_active && (in_pattern || border)) ? 2'b10 : 2'b00;  // slightly less green
+    assign B = 2'b00;                                                     // blue off
+
+
 endmodule
+
