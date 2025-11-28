3cd535d90ed3ce1b600f9fbb57a734032daa1d83
b0806 <tambrendan05@gmail.com>
2025-11-28 10:36:09 -0500

    test

--- test/test.py
+++ test/test.py
@@ -67,11 +67,14 @@ async def accumulator_test(dut):
         new_u = dut.user_project.curr_u.value.signed_integer
         new_v = dut.user_project.curr_v.value.signed_integer
 
+        pix_x = dut.user_project.hvsync_gen.hpos.value.integer
+        pix_y = dut.user_project.hvsync_gen.vpos.value.integer
+
         assert abs(new_u - expected_u) <= 1, \
-            f"accumulator U step wrong: old={old_u} new={new_u} expected={expected_u}"
+            f"accumulator U step wrong at {pix_x},{pix_y}: old={old_u} new={new_u} expected={expected_u}"
 
         assert abs(new_v - expected_v) <= 1, \
-            f"accumulator V step wrong: old={old_v} new={new_v} expected={expected_v}"
+            f"accumulator V step wrong at {pix_x},{pix_y}: old={old_v} new={new_v} expected={expected_v}"
 
 @cocotb.test()
 async def vga_signal_test(dut):
