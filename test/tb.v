1c0c6c6c862efd0f00b11a76332923819fb7c9a6
brendtam <tambrendan05@gmail.com>
2026-05-12 12:33:48 -0400

    oops

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-   tt_bad_apple user_project (
+   tt_um_bad_apple user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
